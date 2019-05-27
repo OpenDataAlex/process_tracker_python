726496a6d3637af8c1d7667018818b68d0a37201
Alex Meadows <alexmeadows@bluefiredatasolutions.com>
2019-05-26 20:41:20 -0400

    Fixing build failure on Trace

--- process_tracker/extract_tracker.py
+++ process_tracker/extract_tracker.py
@@ -32,7 +32,7 @@ class ExtractTracker:
         :type status: string
         """
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(os.environ.get('log_level', 'TRACE'))
+        self.logger.setLevel(os.environ.get('log_level', 'ERROR'))
 
         self.data_store = DataStore()
         self.session = self.data_store.session
