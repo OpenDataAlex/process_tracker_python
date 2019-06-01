674e7bc0706a0aad3a162dfddb55bdf0b1799a18
Alex Meadows <alexmeadows@bluefiredatasolutions.com>
2019-06-01 18:08:58 -0400

    Added more robust logging
    Went through and caught up logging.  Now have finer grained logging for
    debugging and troubleshooting.
    
    Also went ahead and bumped version to 0.2.0.  Will not take effect until tag is made.
    
    Closes #22

--- process_tracker/location_tracker.py
+++ process_tracker/location_tracker.py
@@ -24,11 +24,16 @@ class LocationTracker:
         self.location_path = location_path.lower()
 
         if location_name is None:
+            self.logger.info('Location name not provided.  Generating.')
             self.location_name = self.derive_location_name()
         else:
+            self.logger.info('Using provided location name: %s' % location_name)
             self.location_name = location_name
 
         self.location_type = self.derive_location_type()
+
+        self.logger.info('Registering extract location.')
+
         self.location = self.data_store.get_or_create_item(model=Location
                                                            , location_name=self.location_name
                                                            , location_path=location_path
@@ -48,10 +53,11 @@ class LocationTracker:
 
         if "s3" in self.location_path:
             # If the path is an S3 Bucket, prefix to name.
-
+            self.logger.info('Location appears to be s3 related.  Setting prefix.')
             location_prefix = "s3"
 
         if location_prefix is not None:
+            self.logger.info('Location prefix provided.  Appending to location name.')
             location_name = location_prefix + " - "
 
         location_name += basename(normpath(self.location_path))
@@ -66,10 +72,15 @@ class LocationTracker:
 
         if "s3" in self.location_path or "s3" in self.location_name:
 
+            self.logger.info('Location appears to be s3 related.  Setting type to s3.')
+
             location_type = self.data_store.get_or_create_item(model=LocationType
                                                                , location_type_name="s3")
 
         else:
+
+            self.logger.info('Location did not match special types.  Assuming local directory path.')
+
             location_type = self.data_store.get_or_create_item(model=LocationType
                                                                , location_type_name="local directory")
 
