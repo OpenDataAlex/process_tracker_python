3299820cc94b0af93c0df534e9ada4ca612c3cfa
Alex Meadows <alexmeadows@bluefiredatasolutions.com>
2019-06-19 18:17:07 -0400

    process_tracker_python-51 Sources/Targets need tracking by individual objects
    :sparkles:  Processes can track data source/target as well as source/target objects.
    
    Added ability to track source and target objects as optional addition.
    
    Closes #51

--- process_tracker/models/source.py
+++ process_tracker/models/source.py
@@ -1,7 +1,8 @@
 # SQLAlchemy Models
 # Models for Source entities
 
-from sqlalchemy import Column, Integer, Sequence, String
+from sqlalchemy import Column, ForeignKey, Integer, Sequence, String, UniqueConstraint
+from sqlalchemy.orm import relationship
 
 from process_tracker.models.model_base import Base
 
@@ -22,3 +23,36 @@ class Source(Base):
     def __repr__(self):
 
         return "<Source (name=%s)>" % self.source_name
+
+
+class SourceObject(Base):
+
+    __tablename__ = "source_object_lkup"
+    __table_args__ = {"schema": "process_tracker"}
+
+    source_object_id = Column(
+        Integer,
+        Sequence("source_object_lkup_source_object_id_seq", schema="process_tracker"),
+        primary_key=True,
+        nullable=False,
+    )
+    source_id = Column(
+        Integer, ForeignKey("process_tracker.source_lkup.source_id"), nullable=False
+    )
+    source_object_name = Column(String(250), nullable=False)
+
+    UniqueConstraint(source_id, source_object_name)
+
+    source_processes = relationship(
+        "ProcessSourceObject", back_populates="objects", passive_deletes="all"
+    )
+    target_processes = relationship(
+        "ProcessTargetObject", back_populates="objects", passive_deletes="all"
+    )
+
+    def __repr__(self):
+
+        return "<SourceObject (source_id=%s, source_object_name=%s)>" % (
+            self.source_id,
+            self.source_object_name,
+        )
