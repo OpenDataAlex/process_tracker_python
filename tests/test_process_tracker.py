4cac105f26b2fe96b2fb76dec87b920993b6f2ab
Alex Meadows <alex.meadows@bluefiredatasolutions.com>
2019-11-22 10:31:22 -0500

    process_tracker_python-139 Add high level dates to process
    :bug: forgot to remove milliseconds for mysql
    
    Closes #139

--- tests/test_process_tracker.py
+++ tests/test_process_tracker.py
@@ -1563,6 +1563,7 @@ class TestProcessTracker(unittest.TestCase):
         :return:
         """
         end_date = datetime.now()
+        end_date.replace(microsecond=0)
         self.process_tracker.change_run_status(
             new_status="completed", end_date=end_date
         )
